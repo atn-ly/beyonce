2d7d656fe2bdf7c9325ef48b7c0888f22ad79844
Tam Ly <annatn.ly@mail.utoronto.ca>
2024-04-13 17:48:45 -0400

    Add simulated data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,47 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Simulates a dataset of number of Spotify streams and number of weeks on the Billboard Hot 100.
+# Author: Tam Ly
+# Date: 18 April 2024
+# Contact: annatn.ly@mail.utoronto.ca
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(302)
 
+beyonce_simulation <-
+  tibble(
+    number_weeks_on_chart = runif(min = 0, max = 20, n = 100) |> round(0),
+    number_streams = rpois(n = 100, lambda = 15)
+  )
 
+beyonce_simulation <-
+  beyonce_simulation |> 
+  mutate(
+    number_weeks_on_chart = as.integer(number_weeks_on_chart)
+    )
 
+beyonce_simulation |>
+  ggplot(aes(y = number_streams, x = number_weeks_on_chart)) +
+  geom_point() +
+  labs(
+    x = "Number of weeks on Billboard Hot 100",
+    y = "Number of Spotify streams (in millions)"
+  ) +
+  theme_classic() +
+  scale_fill_brewer(palette = "Set1")
+
+#### Test simulated data ####
+#Tests of class:
+beyonce_simulation$number_weeks_on_chart |> class() == "integer"
+beyonce_simulation$number_streams |> class() == "integer"
+
+#Tests of number of observations:
+beyonce_simulation$number_weeks_on_chart |> min() >= 0
+beyonce_simulation$number_weeks_on_chart |> max() <= 52
+beyonce_simulation$number_streams |> min() >= 1
+beyonce_simulation$number_streams |> max() <= 2000
