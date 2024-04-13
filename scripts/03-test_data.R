6fbbf00226917425794a20f83642e3c37e85a3a2
Tam Ly <annatn.ly@mail.utoronto.ca>
2024-04-13 19:08:59 -0400

    Add tests for data and model

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,36 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Tests the cleaned Beyonce data.
+# Author: Tam Ly
+# Date: 18 April 2024
+# Contact: annatn.ly@mail.utoronto.ca
+# Pre-requisites: Run 02-data_cleaning and 04-model
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(arrow)
+
+
+#### Read data ####
+beyonce_cleaned <- read_parquet("data/analysis_data/beyonce_cleaned.parquet")
+
+#### Read model ####
+beyonce_pois <- readRDS(file = here::here("models/beyonce_pois.rds"))
+beyonce_nbinom <- readRDS(file = here::here("models/beyonce_nbinom.rds"))
 
 #### Test data ####
+#Tests of class:
+beyonce_cleaned$wks_on_chart |> class() == "integer"
+beyonce_cleaned$spotify_streams |> class() == "integer"
+
+#Tests of number of observations:
+beyonce_cleaned$wks_on_chart |> min() >= 0
+beyonce_cleaned$wks_on_chart |> max() <= 52
+beyonce_cleaned$spotify_streams |> min() >= 1
+beyonce_cleaned$spotify_streams |> max() <= 2000000000
+
+#### Test model ####
+#Tests of coefficients
+between(beyonce_pois$coefficients[1], 0, 20)
+between(beyonce_pois$coefficients[2], 0, 20)
