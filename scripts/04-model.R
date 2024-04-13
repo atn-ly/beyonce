7299e0f18ef4cc1febfe3af71ac497af13430fa4
Tam Ly <annatn.ly@mail.utoronto.ca>
2024-04-13 19:06:16 -0400

    Add poisson and negative binomial models

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,37 +1,49 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models number of Spotify streams based on number of weeks on the Billboard Hot 100.
+# Author: Tam Ly
+# Date: 18 April 2024
+# Contact: annatn.ly@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Run 02-data_cleaning.R
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(arrow)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+beyonce_cleaned <- read_parquet("data/analysis_data/beyonce_cleaned.parquet")
 
 ### Model data ####
-first_model <-
+beyonce_pois <-
   stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
+    spotify_streams ~ wks_on_chart,
+    data = beyonce_cleaned,
+    family = poisson(link = "log"),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
+    seed = 302
   )
 
+beyonce_nbinom <-
+  stan_glm(
+    spotify_streams ~ wks_on_chart,
+    data = beyonce_cleaned,
+    family = neg_binomial_2(link = "log"),
+    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    seed = 302
+  )
 
 #### Save model ####
 saveRDS(
-  first_model,
-  file = "models/first_model.rds"
+  beyonce_pois,
+  file = "models/beyonce_pois.rds"
 )
 
-
+saveRDS(
+  beyonce_nbinom,
+  file = "models/beyonce_nbinom.rds"
+)
